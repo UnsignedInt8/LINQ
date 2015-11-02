1ecfbc32960123935b3f4592b11ab5f6ab22dd14
Neko <sunshiny.neko@gmail.com>
2015-11-02 17:24:57 +0800

    Updated test cases

--- test/String.js
+++ test/String.js
@@ -23,4 +23,49 @@ describe('test String linqable', () => {
     assert.strictEqual(s.aggregate((c, n) => c + n), s);
     assert.strictEqual(s.aggregate('xx', (c, n) => c + n), 'xx' + s);
   });
+  
+  it('average', () => {
+    assert(isNaN(s.average()));
+  });
+  
+  it('concat', () => {
+    assert.deepEqual(s.concatenate('mxmx').toArray(), Array.from(s + 'mxmx'));
+  });
+  
+  it('contains', () => {
+    assert(s.contains('N'));
+    assert(!s.contains(0));
+  });
+  
+  it('count', () => {
+    assert(s.count() === s.length);
+    assert(''.count() === ''.length);
+  });
+  
+  it('defaultIfEmpty', () => {
+    assert(s.defaultIfEmpty().count() === s.length);
+    assert.deepEqual(''.defaultIfEmpty(0).toArray(), [0]);
+  });
+  
+  it('distinct', () => {
+    assert.deepEqual(s.distinct().toArray(), Array.from('Helo wrd,hLINQ!0'));
+  });
+  
+  it('elementAt', () => {
+    assert(s.elementAt(5) === ' ');
+  });
+  
+  it('except', () => {
+    assert.deepEqual(s.except('o').toArray(), Array.from('Hell wrld, hell LINQ! 0'));
+  });
+  
+  it('first', () => {
+    assert.strictEqual(s.first(), 'H');
+    assert.strictEqual(s.first(i => i == 0), ' ');
+  });
+  
+  it('single', () => {
+    assert.strictEqual(s.single(i => i === '0'), '0');
+    assert.throws(() => s.single(i => i == 'l'), Error);
+  })
 });
