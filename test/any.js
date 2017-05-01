7bb3d5527362b95d9c17c5f614c015b2c41e2174
UnsignedInt8 <unsignedint8@gmail.com>
2017-05-01 16:50:06 +0800

    update testcases

--- test/any.js
+++ test/any.js
@@ -5,25 +5,31 @@ let assert = require('assert');
 
 describe('test any operator', () => {
   it('should return false', () => {
-    let r = [0, 0, 0, 0, 0].any((i) => i !==0);
+    let r = [0, 0, 0, 0, 0].any((i) => i !== 0);
     assert.equal(r, false);
   });
-  
+
   it('should return true', () => {
     let r = [1, 0, 0, 'x'].any((i) => i === 'x');
     assert(r);
   });
-  
+
   it('should return false', () => {
     let r = [].any(i => false);
     assert.equal(r, false);
   });
-  
+
   it('should be true', () => {
     assert.equal([1].any(), true);
   });
-  
+
   it('should be false', () => {
     assert.equal([].any(), false);
   });
+
+  it('array & any', () => {
+    let a1 = [1, 2, 3];
+    assert.equal(a1.any(), true);
+    assert.deepEqual(a1, [1, 2, 3]);
+  })
 })
