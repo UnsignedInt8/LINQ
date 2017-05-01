207e5f9f9bc9922c8143496817e2a4e016230dca
UnsignedInt8 <unsignedint8@gmail.com>
2017-05-01 10:54:49 +0800

    Update testcases

--- kinq.js
+++ kinq.js
@@ -942,15 +942,17 @@ let KINQ = function (options) {
   let linq = {};
   linqOperators.forEach((item) => linq[item.name] = item);
 
-  let iterableObjects = [toLinqable.prototype,
-  Array.prototype,
-  Map.prototype,
-  Set.prototype,
-  WeakMap.prototype,
-  WeakSet.prototype,
-  String.prototype,
-  Buffer.prototype
+  let iterableObjects = [
+    toLinqable.prototype,
+    Array.prototype,
+    Map.prototype,
+    Set.prototype,
+    WeakMap.prototype,
+    WeakSet.prototype,
+    String.prototype,
+    Buffer.prototype
   ];
+
   iterableObjects.forEach(item => Object.assign(item, linq));
 
 }
--- test/select.js
+++ test/select.js
@@ -5,8 +5,22 @@ require('../index')();
 
 describe('test select operator', () => {
   let n = [1, 2, 3, -1, 2, 1];
-  
+
   it('should be equal', () => {
     assert.deepEqual(n.select(i => i * 2).toArray(), n.map(i => i * 2));
+  });
+
+  it('selects map', () => {
+    let m1 = new Map();
+    m1.set('a', 1);
+    m1.set('b', 2);
+    m1.set('z', 3);
+
+    let m2 = new Map();
+    m2.set('b', 2);
+    m2.set('c', 3);
+
+    let result = m1.except(m2, ([k1, v1], [k2, v2]) => k1 === k2).select(item => item[0]).toArray();
+    assert.deepEqual(result, ['a', 'z']);
   });
 });
